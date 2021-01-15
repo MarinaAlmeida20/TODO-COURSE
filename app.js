aeee8f21bcb5066e3aef44b117c6aba81a0be90f
Marina <marinabrito.ma@gmail.com>
2021-01-15 22:18:41 +0000

    js and css

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//Selectors
+//Select DOM
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
@@ -8,15 +8,28 @@ todoButton.addEventListener('click', addTodo);
 
 //Functions
 
-function addTodo(event){
-    //Prevent form from submitting
-    event.preventDefault();
-    // div
+function addTodo(e) {
+    //Prevent natural behaviour
+    e.preventDefault();
+    //Create todo div
     const todoDiv = document.createElement("div");
     todoDiv.classList.add("todo");
-    // Create Li
+    //Create li
     const newTodo = document.createElement("li");
-    newTodo.innerText = 'hey';
+    newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo);
+    //Create Completed Button
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = `<i class="fas fa-check"></i>`;
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    // Check trash button
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = `<i class="fas fa-trash"></i`;
+    trashButton.classList.add("trash-btn");
+    // Append to list
+    todoList.appendChild(todoDiv);
+    // Clear Todo Input Value
+    todoInput.value = '';
 }
